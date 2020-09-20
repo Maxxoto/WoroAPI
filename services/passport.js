a213a9669d85253cac942de5a339b5f3e5359a58
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2020-09-20 14:23:51 +0700

    added passReqToCallback

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,7 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
       proxy: true,
-      // passReqToCallback : true
+      passReqToCallback: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       // console.log("Profile : ",profile)
