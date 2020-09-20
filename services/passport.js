ad04143818db31413716f24aa1a0a8e98bedacca
Ahmat Dani Setiawan <ahmatdanis24@gmail.com>
2020-09-20 13:07:29 +0700

    adding proxy true to passport js

--- services/passport.js
+++ services/passport.js
@@ -21,7 +21,7 @@ passport.use(
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
       callbackURL: "/auth/google/callback",
-      // proxy : true
+      proxy: true,
       // passReqToCallback : true
     },
     async (accessToken, refreshToken, profile, done) => {
